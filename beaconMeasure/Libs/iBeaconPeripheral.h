aeb3695e5e431dbd5c8dbfad54b0de1400f960b5
加藤 雄大 <takahiro.kato15@gmail.com>
2015-04-19 13:58:57 +0900

    iBeaconの発信側画面の作成
    - iBeaconの発信処理を実装

--- beaconMeasure/Libs/iBeaconPeripheral.h
+++ beaconMeasure/Libs/iBeaconPeripheral.h
@@ -22,4 +22,31 @@
 /// デリゲート先で参照するためのプロパティ
 @property (nonatomic, assign) id<iBeaconPeripheralDelegate> delegate;
 
+#pragma mark - init
+/** 
+ 初期化処理
+ @return iBeaconPeripheralオブジェクト
+ */
+- (id)init;
+
+/**
+ iBeaconのプロパティを付与する初期化処理
+ @param uuid iBeaconのUUID
+ @param major iBeaconのmajor
+ @param minor iBeaconのminor
+ @return iBeaconPeripheralオブジェクト
+ */
+- (id)initWithUUID:(NSString *)uuid major:(NSInteger)major minor:(NSInteger)minor;
+
+#pragma mark - other method
+/**
+ iBeacon信号を発信する処理
+ */
+- (void)startAdvertising;
+
+/**
+ iBeacon信号の発信を停止する処理
+ */
+- (void)stopAdvertising;
+
 @end
