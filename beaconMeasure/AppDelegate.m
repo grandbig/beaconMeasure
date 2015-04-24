08fdf4374ebcb32d63ce9c010ae0aba9b23d07d9
加藤 雄大 <takahiro.kato15@gmail.com>
2015-04-24 23:11:27 +0900

    スプラッシュ画面の変更
    - iOS8用のスプラッシュ画面を作成しました

--- beaconMeasure/AppDelegate.m
+++ beaconMeasure/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    sleep(3);
+    
     return YES;
 }
 
